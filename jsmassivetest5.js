91c936099b46c0c0c32d64ac17e974b699135483
IlyaGab <ilyagab1994@gmail.com>
2022-02-02 21:53:32 +0300

    feat: add fifth massive task

---
+++ jsmassivetest5.js
@@ -0,0 +1,18 @@
+let obj = {
+	1: 125,
+	2: 225,
+	3: 128,
+	4: 356,
+	5: 145,
+	6: 281,
+	7: 452,
+};
+let arr = [];
+
+for (let key in obj){
+     if (String(obj[key])[0] === '1' || (String(obj[key])[0] === '2')) {
+         arr = obj[key];
+         console.log(arr);
+     }
+     
+}
