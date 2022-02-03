224e4be57cee7298c78edf04682bc8a6c28057cd
IlyaGab <ilyagab1994@gmail.com>
2022-02-03 20:11:51 +0300

    feat:add multidimensional object task2

---
+++ 07.js
@@ -0,0 +1,35 @@
+let months = {
+	'ru': [
+		'январь',
+		'февраль',
+		'март',
+		'апрель',
+		'май',
+		'июнь',
+		'июль',
+		'август',
+		'сентябрь',
+		'октябрь',
+		'ноябрь',
+		'декабрь',
+	],
+	'en': [
+		'january',
+		'february',
+		'march',
+		'april',
+		'may',
+		'june',
+		'july',
+		'august',
+		'september',
+		'october',
+		'november',
+		'december',
+	],
+};
+
+let lang = 'en'; 
+let month = 5; 
+
+console.log(months[lang][month-1]);
