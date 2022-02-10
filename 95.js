73e54829b07f0b9da3a0885ae60419de0c263595
IlyaGab <ilyagab1994@gmail.com>
2022-02-10 20:14:37 +0300

    feat: add object Date task15

---
+++ 95.js
@@ -0,0 +1,6 @@
+let now  = new Date();
+let date = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 24);
+
+let diff = date - now;
+
+console.log((diff / (1000 * 60 * 60)).toPrecision(3));
