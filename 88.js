f03b42d4375616b34da8016169f5ce60775db06f
IlyaGab <ilyagab1994@gmail.com>
2022-02-10 19:13:51 +0300

    feat: add object Date task8

---
+++ 88.js
@@ -0,0 +1,5 @@
+let myBirthday = new Date(1994, 4, 6);
+let nowDate = new Date();
+let diff = nowDate.getTime() - myBirthday.getTime();
+
+console.log((diff / (1000 * 60 * 60 * 24 * 30.5 * 12)).toPrecision(3) + " " + "years");
