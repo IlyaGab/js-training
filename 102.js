0897ef41393035d146d3b7f11f065f99868d7a42
IlyaGab <ilyagab1994@gmail.com>
2022-02-11 13:54:12 +0300

    feat: add receiving DOM element task1

---
+++ 102.js
@@ -0,0 +1,7 @@
+let paragraph1 = document.querySelector('#elem1');
+let paragraph2 = document.querySelector('#elem2');
+let paragraph3 = document.querySelector('#elem3');
+
+console.log(paragraph1);
+console.log(paragraph2);
+console.log(paragraph3);
