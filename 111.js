1419f3fa96d6c1ce1eb9458ede11524254d700ee
IlyaGab <ilyagab1994@gmail.com>
2022-02-11 14:25:34 +0300

    fead: add EventListener task6

---
+++ 111.js
@@ -0,0 +1,17 @@
+let paragraph = document.querySelector('#elem');
+paragraph.addEventListener('click', func1);
+paragraph.addEventListener('click', func2);
+paragraph.addEventListener('click', func3);
+
+
+function func1() {
+	console.log('1');
+};
+
+function func2() {
+	console.log('2');
+};
+
+function func3() {
+	console.log('3');
+};
