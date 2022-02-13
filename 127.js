74021f3a8ffd80989774fad1b081a9f34e588d1e
IlyaGab <ilyagab1994@gmail.com>
2022-02-13 17:48:40 +0300

    feat: add eventListener task22

---
+++ 127.js
@@ -0,0 +1,9 @@
+let elem = document.querySelector('#elem');
+
+elem.addEventListener('focus', function(){
+    elem.value = 1;
+});
+
+elem.addEventListener('blur', function(){
+    elem.value = 2;
+});
