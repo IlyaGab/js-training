80e1c47c32140502f8027964067b275d94b479bd
IlyaGab <ilyagab1994@gmail.com>
2022-02-13 19:22:00 +0300

    feat: add eventListener task35

---
+++ 140.js
@@ -0,0 +1,9 @@
+let elems = document.querySelectorAll('.elem');
+
+for(let elem of elems){
+    elem.addEventListener('blur', func);
+};
+
+function func(){
+    this.value = Number(this.value) + 1;
+};
