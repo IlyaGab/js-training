8191d3eb66905ec11fa0d8eb200d0277b28ce511
IlyaGab <ilyagab1994@gmail.com>
2022-02-14 16:41:34 +0300

    feat: add custom attributes task5

---
+++ 164.js
@@ -0,0 +1,8 @@
+let elem = document.querySelector('#elem');
+
+elem.addEventListener('blur', function(){
+    if (elem.value.length < elem.dataset.min || elem.value.length > elem.dataset.max){
+        console.log('Error');
+    };
+});
+
