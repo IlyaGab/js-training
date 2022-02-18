3bb66a083573dd9d969f5fb446085749c975b193
IlyaGab <ilyagab1994@gmail.com>
2022-02-18 15:08:26 +0300

    feat: add stop bubbling events task1

---
+++ 219.js
@@ -0,0 +1,12 @@
+let parent = document.querySelector('#parent');
+let button = document.querySelector('button');
+let block  = document.querySelector('#block');
+
+button.addEventListener('click', function(event) {
+	block.classList.add('active');
+    event.stopImmediatePropagation();
+});
+
+parent.addEventListener('click', function() {
+	block.classList.remove('active');
+});
