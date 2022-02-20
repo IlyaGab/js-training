3f2c4d884d9e659a5f35ffd27e8ed0b3cb3a08d7
IlyaGab <ilyagab1994@gmail.com>
2022-02-20 13:38:27 +0300

    feat: add context task2

---
+++ 222.js
@@ -0,0 +1,11 @@
+"use strict";
+
+let elem = document.querySelector('#elem');
+elem.addEventListener('blur', func);
+
+function func() {
+	alert( square(this) );
+	function square(param) {
+		return param.value * param.value;
+	};
+};
