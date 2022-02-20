1c9269494e167707394e50fdef39756d3b463850
IlyaGab <ilyagab1994@gmail.com>
2022-02-20 13:56:41 +0300

    feat: add context binding with the call method task1

---
+++ 224.js
@@ -0,0 +1,13 @@
+"use strict"
+
+let elem1 = document.querySelector('#elem1');
+let elem2 = document.querySelector('#elem2');
+let elem3 = document.querySelector('#elem3');
+
+function func(){
+    console.log(this.value);
+};
+
+func.call(elem1);
+func.call(elem2);
+func.call(elem3);
