945c44375fffe55011e19b42917ddaeb03d61041
IlyaGab <ilyagab1994@gmail.com>
2022-02-25 21:40:32 +0300

    refactor: fix task

--- 284.js
+++ 284.js
@@ -1,8 +1,7 @@
 let array = [1, 2, 'Dog', 5, 6, 15];
-let list = document.createElement('ul');
+let list = document.querySelector('ul');
 for(let i = 0; i < array.length; i++){
     let item = document.createElement('li');
     item.innerHTML = array[i];
     list.append(item);
 };
-console.log(list);
