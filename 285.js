38aedb9ba169c7651829e7639fdc98e9441217c6
IlyaGab <ilyagab1994@gmail.com>
2022-02-25 21:59:35 +0300

    feat: add cahnge elements practice task2

---
+++ 285.js
@@ -0,0 +1,17 @@
+let array = [1, 2, 'Dog', 5, 6, 15];
+let list = document.querySelector('ul');
+for(let i = 0; i < array.length; i++){
+    let item = document.createElement('li');
+    item.innerHTML = array[i];
+    list.append(item);
+    item.addEventListener('click', function func(){
+        let input = document.createElement('input');
+        item.innerHTML = '';
+        item.appendChild(input);
+        input.addEventListener('blur', function(){
+            item.innerHTML = this.value;
+            item.addEventListener('click', func);
+        });
+        item.removeEventListener('click', func);
+    });
+};  
