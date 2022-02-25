c0621dbe25c441e149978e33eddf668bedabe969
IlyaGab <ilyagab1994@gmail.com>
2022-02-25 21:38:21 +0300

    feat: add change elements task1

---
+++ 284.js
@@ -0,0 +1,8 @@
+let array = [1, 2, 'Dog', 5, 6, 15];
+let list = document.createElement('ul');
+for(let i = 0; i < array.length; i++){
+    let item = document.createElement('li');
+    item.innerHTML = array[i];
+    list.append(item);
+};
+console.log(list);
