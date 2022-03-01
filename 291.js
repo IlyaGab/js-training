5eb5357a56b638ddf643df6111bdc6672e905835
IlyaGab <ilyagab1994@gmail.com>
2022-03-01 19:11:45 +0300

    feat: add functions for working with the dom task4

---
+++ 291.js
@@ -0,0 +1,10 @@
+function forEach(selector, func){
+    let elems = document.querySelectorAll(selector);
+    for(let i = 0; i  < elems.length; i++){
+        func(elems[i], i);
+    };
+};
+
+forEach('.elem', function(elem, index){
+    elem.innerHTML = elem.innerHTML + (index + 1);
+});
